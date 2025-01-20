2a89312724194b49e94a86af3cc5852f31354382
aravind <aravind284479@gmail.com>
2025-01-20 20:27:22 +0530

    feat:Profile Edit

--- components/Auth/WithProviders.tsx
+++ components/Auth/WithProviders.tsx
@@ -1,44 +1,24 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import Button from "../ui/Button";
 import { signIn } from "next-auth/react";
 
 function WithProviders() {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string|null>(null);
-
-  const signWithGoogle = async () => {
-    setLoading(true);
-    const res = await signIn("google", { callbackUrl: "/" });
-    if (res?.error) {
-      setError(res.error);
-    }
-    setLoading(false);
+  const signWithGoogle = () => {
+    signIn("google", { callbackUrl: "/", redirect: true });
   };
-
-  const signWithGithub = async () => {
-    setLoading(true);
-    const res = await signIn("github", { callbackUrl: "/" });
-    if (res?.error) {
-      setError(res.error);
-    }
-    setLoading(false);
+  const signWithGithub = () => {
+    signIn("github", { callbackUrl: "/", redirect: true });
   };
-
   return (
     <div className="flex justify-center items-center h-dvh flex-col gap-5">
       <div className="flex flex-col items-center">
-        <p className="text-2xl">CodeX Media</p>
-        <p className="text-[10px]">A Social Network for Developers</p>
+        <p className="text-2xl ">CodeX Media</p>
+        <p className="text-[10px]">A Social Nework for Developer</p>
       </div>
-      {error && <p className="text-red-500">{error}</p>}
-      <Button onClick={signWithGoogle} disabled={loading}>
-        {loading ? "Signing in with Google..." : "Continue with Google"}
-      </Button>
-      <p className="text-gray-500">OR</p>
-      <Button onClick={signWithGithub} disabled={loading}>
-        {loading ? "Signing in with GitHub..." : "Continue with GitHub"}
-      </Button>
+      <Button onClick={signWithGoogle}>Continue with Google</Button>
+      <p className=" text-gray-500">OR</p>
+      <Button onClick={signWithGithub}>Continue with Github</Button>
     </div>
   );
 }
