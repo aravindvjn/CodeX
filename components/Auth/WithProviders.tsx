aab3f09b93847c7b8bf43c81be15f656e4e9e24b
aravind <aravind284479@gmail.com>
2025-01-20 20:23:44 +0530

    feat:Profile Edit

--- components/Auth/WithProviders.tsx
+++ components/Auth/WithProviders.tsx
@@ -1,25 +1,44 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Button from "../ui/Button";
 import { signIn } from "next-auth/react";
-import { callBackURL } from "@/globals/constants/constants";
 
 function WithProviders() {
-  const signWithGoogle = () => {
-    signIn("google", { callbackUrl: callBackURL || "/" });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string|null>(null);
+
+  const signWithGoogle = async () => {
+    setLoading(true);
+    const res = await signIn("google", { callbackUrl: "/" });
+    if (res?.error) {
+      setError(res.error);
+    }
+    setLoading(false);
   };
-  const signWithGithub = () => {
-    signIn("github", { callbackUrl: callBackURL || "/" });
+
+  const signWithGithub = async () => {
+    setLoading(true);
+    const res = await signIn("github", { callbackUrl: "/" });
+    if (res?.error) {
+      setError(res.error);
+    }
+    setLoading(false);
   };
+
   return (
     <div className="flex justify-center items-center h-dvh flex-col gap-5">
       <div className="flex flex-col items-center">
-        <p className="text-2xl ">CodeX Media</p>
-        <p className="text-[10px]">A Social Nework for Developer</p>
+        <p className="text-2xl">CodeX Media</p>
+        <p className="text-[10px]">A Social Network for Developers</p>
       </div>
-      <Button onClick={signWithGoogle}>Continue with Google</Button>
-      <p className=" text-gray-500">OR</p>
-      <Button onClick={signWithGithub}>Continue with Github</Button>
+      {error && <p className="text-red-500">{error}</p>}
+      <Button onClick={signWithGoogle} disabled={loading}>
+        {loading ? "Signing in with Google..." : "Continue with Google"}
+      </Button>
+      <p className="text-gray-500">OR</p>
+      <Button onClick={signWithGithub} disabled={loading}>
+        {loading ? "Signing in with GitHub..." : "Continue with GitHub"}
+      </Button>
     </div>
   );
 }
